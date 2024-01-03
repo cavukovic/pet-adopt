1d55e82efaeee04adde07ed521746f3364973372
Charlie Vukovic <cavukovic@gmail.com>
2024-01-03 13:21:50 -0500

    basic hooks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
             animal="Cat"
             breed="Persian" 
             />
+            <SearchParams />
         </div>
     );
 };
