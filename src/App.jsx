4db0fbd0cb3383032a648581a4e96135a6e429ac
Charlie Vukovic <cavukovic@gmail.com>
2024-01-04 13:23:24 -0500

    react queries added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import ReactDOM from "react-dom/client";
 import {Link} from "react-router-dom";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+    queries: {
+      staleTime: Infinity, // makes cache time for whole session
+      cacheTime: Infinity,
+    }
+  }
+});
 
 const App = () => {
   return (
       <BrowserRouter>
-      <header> 
-        <Link to = "/">Adopt a pet</Link>
-      </header>
-        <div>
-            <Routes>
-              <Route path="/details/:id" element={<Details/>} /> 
-              <Route path="/" element={<SearchParams/>} />
-            </Routes>
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <header> 
+            <Link to = "/">Adopt a pet</Link>
+          </header>
+          <div>
+              <Routes>
+                <Route path="/details/:id" element={<Details/>} /> 
+                <Route path="/" element={<SearchParams/>} />
+              </Routes>
+          </div>
+          </QueryClientProvider>
       </BrowserRouter>
     );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,33 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     const {id} = useParams(); // use params gets the id from the browser router 
+    const results = useQuery(["details", id], fetchPet);
+
+    if(results.isLoading){
+        return(
+            <div className="loading-pane">
+                <h2 className = "loader">꩜</h2>
+            </div>
+        )
+    }
+
+    // we can assume at this point on that pet is loaded
+
+    const pet = results.data.pets[0];
+
     return (
-        <div>
-            <h2>{id}</h2> 
+        <div className = "details">
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+                    <hr></hr>
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
         </div>
     )
 };
